7debc847fe6ef95cb211ddd39b9f839fde82d0ce
koshtony <tonykungu2@gmail.com>
2024-01-21 13:51:50 +0300

    reset to structural setup

--- tracker/tracker/settings.py
+++ tracker/tracker/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--q9dns!=!53)6yfuj#7e$$e%2j9g9k^mt*g*qgye!j9!(pafs0'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
